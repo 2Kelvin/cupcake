f0475063c33d3f1cfecf8d2521ebe56cc861b16e
2Kelvin <rocketmancodes02@gmail.com>
2024-06-01 11:18:28 +0300

    add a navhost + define all routes

--- app/src/main/java/com/example/cupcake/CupcakeScreen.kt
+++ app/src/main/java/com/example/cupcake/CupcakeScreen.kt
@@ -1,5 +1,8 @@
 package com.example.cupcake
 
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -14,11 +17,26 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.cupcake.data.DataSource
+import com.example.cupcake.ui.OrderSummaryScreen
 import com.example.cupcake.ui.OrderViewModel
+import com.example.cupcake.ui.SelectOptionScreen
+import com.example.cupcake.ui.StartOrderScreen
+
+enum class CupcakeScreen {
+    Start,
+    Flavor,
+    Pickup,
+    Summary
+}
 
 /**
  * Composable that displays the topBar and displays back button if back navigation is possible.
@@ -65,5 +83,57 @@ fun CupcakeApp(
     ) { innerPadding ->
         val uiState by viewModel.uiState.collectAsState()
 
+        // navHost composable displays other composable (destinations) depending on the route
+        NavHost(
+            navController = navController, // used to navigate between screens using the navigate() method
+            startDestination = CupcakeScreen.Start.name, // first screen of the app | default destination
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            // composable takes in 2 parameters
+            // 'route' and the trailing lambda function 'content' (composable for the route)
+            // displaying 'StartOrderScreen' for the route 'CupcakeScreen.Start.name'
+            composable(route = CupcakeScreen.Start.name) {
+                StartOrderScreen(
+                    quantityOptions = DataSource.quantityOptions,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(dimensionResource(R.dimen.padding_medium))
+                )
+            }
+
+            // displaying 'SelectOptionScreen' for the route 'CupcakeScreen.Flavor.name'
+            composable(route = CupcakeScreen.Flavor.name) {
+                // context will allow to access string resource ids defined in the app resources
+                // to get the list of flavors from the viewModel as string resources
+                val context = LocalContext.current
+
+                SelectOptionScreen(
+                    subtotal = uiState.price,
+                    options = DataSource.flavors.map { id ->
+                        context.resources.getString(id)
+                    },
+                    onSelectionChanged ={ viewModel.setFlavor(it) },
+                    modifier = Modifier.fillMaxHeight()
+                )
+            }
+
+            // displaying 'SelectOptionScreen' for the route 'CupcakeScreen.Pickup.name'
+            composable(route = CupcakeScreen.Pickup.name) {
+                SelectOptionScreen(
+                    subtotal = uiState.price,
+                    options = uiState.pickupOptions,
+                    onSelectionChanged ={ viewModel.setDate(it) },
+                    modifier = Modifier.fillMaxHeight()
+                )
+            }
+
+            // displaying 'OrderSummaryScreen' for the route 'CupcakeScreen.Summary.name'
+            composable(route = CupcakeScreen.Summary.name) {
+                OrderSummaryScreen(
+                    orderUiState = uiState,
+                    modifier = Modifier.fillMaxHeight()
+                )
+            }
+        }
     }
 }
