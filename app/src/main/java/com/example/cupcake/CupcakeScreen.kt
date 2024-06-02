96a76dfe27badfe85188696476ace180275e39f2
2Kelvin <rocketmancodes02@gmail.com>
2024-06-02 07:55:33 +0300

    create a share intent

--- app/src/main/java/com/example/cupcake/CupcakeScreen.kt
+++ app/src/main/java/com/example/cupcake/CupcakeScreen.kt
@@ -1,5 +1,7 @@
 package com.example.cupcake
 
+import android.content.Context
+import android.content.Intent
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -140,15 +142,39 @@ fun CupcakeApp(
 
             // displaying 'OrderSummaryScreen' for the route 'CupcakeScreen.Summary.name'
             composable(route = CupcakeScreen.Summary.name) {
+                // getting the app context to share to 'shareOrder' intent
+                val context = LocalContext.current
+
                 OrderSummaryScreen(
                     orderUiState = uiState,
-                    onSendButtonClicked = { subject: String, summary: String ->},
+                    onSendButtonClicked = { subject: String, summary: String ->
+                        shareOrder(context, subject = subject, summary = summary)
+                    },
                     onCancelButtonClicked = { cancelOrderAndNavigateToStart(viewModel, navController) },
                     modifier = Modifier.fillMaxHeight()
                 )
             }
         }
     }
+}
+
+// share to other apps intent
+private fun shareOrder(
+    context: Context,
+    subject: String,
+    summary: String,
+) {
+    // an intent to create a new activity to share info to other apps
+    // this activity appears at the bottom of the screen & on top of our app
+    val intent = Intent(Intent.ACTION_SEND).apply {
+        type = "text/plain"
+        putExtra(Intent.EXTRA_SUBJECT, subject)
+        putExtra(Intent.EXTRA_TEXT, summary)
+    }
+
+    context.startActivity(
+        Intent.createChooser(intent, context.getString(R.string.new_cupcake_order))
+    )
 }
 
 // cancel buttons logic
